1409f47c10ffae3ab5edaccacb670c5744b226c4
saeed5430 <abdollahi003@gmail.com>
2026-09-16 12:50:35 +0330

    make a better ui/ux

--- frontend/src/components/shop/ProductGrid.tsx
+++ frontend/src/components/shop/ProductGrid.tsx
@@ -26,7 +26,7 @@ export default function ProductGrid({
 
   return (
 
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
 
       {
         products.map((product) => (
